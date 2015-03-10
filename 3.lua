b632e64c5ecdf02c514df2c6768eb9a86cadfed2
reflechant <reflechant@users.noreply.github.com>
2015-03-10 17:57:38 +0300

    ex.3 unfinished

--- 3.lua
+++ 3.lua
@@ -1,3 +1,44 @@
 #!/usr/bin/env lua
 
 
+local function Fibonacci(n)
+	local F={1,2}
+
+	while F[#F] <= n do
+		table.insert(F,F[#F]+F[#F-1])
+	end
+
+	return F
+end
+
+
+local function isEven(x)
+	if x%2 == 0 then
+		return x
+	end
+end
+
+local function sum(t)
+	local sum = 0
+	for i,v in pairs(t) do
+		sum = sum + v
+	end
+	return sum
+end
+
+local function map(t,func)
+	local T = {}
+	for k,v in pairs(t) do
+		--table.insert(T,func(v))
+		T[k] = func(v)
+	end
+	return T
+end
+
+
+local function main()
+	io.write( "\n" .. sum( map( Fibonacci(4e6), isEven ) ) .. "\n" )
+	
+end
+
+main()
