fa670733333817a3cbd64f45291c3d336c6f1e87
reflechant <reflechant@users.noreply.github.com>
2015-03-11 18:03:14 +0300

    ex.3 finished but ugly

--- 3.lua
+++ 3.lua
@@ -1,44 +1,96 @@
 #!/usr/bin/env lua
 
 
-local function Fibonacci(n)
-	local F={1,2}
+--[[local mt = {
+	__tostring = function (t)
 
-	while F[#F] <= n do
-		table.insert(F,F[#F]+F[#F-1])
+		keys = {}
+		for k,_ in pairs(t) do
+			table.insert(keys,k)
+		end
+		table.sort(keys)
+		
+		return "{"..table.concat(t,",").."}"
 	end
+}]]--
 
+local function isPrime (x,pt)
+	for _,v in ipairs(pt) do
+		if x%v == 0 then return false end
+	end
+	return true
+end
+
+local function getPrime ()
+	local P = {2}
+	local i = 0
+	return function ()
+		local x = P[#P]
+		while true do
+			x = x + 1
+			if isPrime(x,P) then
+				table.insert(P,x)
+				i = i + 1
+				return P[i]
+			end
+		end
+	end
+end
+
+local function divtimes (n,x)
+	local t = 0
+	while n%x == 0 do
+		n = n / x
+		t = t + 1
+	end
+	if t == 0 then t = nil end
+	return t,n
+end
+
+local function getFactors (n,prgen)
+	local F = {}
+	local pr = prgen()
+	
+	while n>1 do
+		local x = pr()
+		F[x],n = divtimes(n,x)
+		--[[a,b = divtimes(n,x)
+		n = b
+		table.insert(F,{x,a})]]--
+		--print(x,F[x],n)
+		--print(F)
+	end
+	--setmetatable(F,mt)
 	return F
 end
 
-
-local function isEven(x)
-	if x%2 == 0 then
-		return x
+--[[local function printF (t)
+	io.write("\n{")
+	for  in pairs(t) do
+		io.write("{")
 	end
+	io.write("}\n")
+end]]--
+
+--[[pr = getPrime()
+P = {}
+setmetatable(P,mt)
+for i = 1,15 do
+	--table.insert(P,pr())
+	P[i] = pr()
 end
+print(P)]]--
 
-local function sum(t)
-	local sum = 0
-	for i,v in pairs(t) do
-		sum = sum + v
-	end
-	return sum
+--print( getFactors(600851475143,getPrime ) )
+t = getFactors(600851475143,getPrime )
+
+local keys = {}
+for k,_ in pairs(t) do
+	table.insert(keys,k)
 end
+table.sort(keys)
+print(keys[#keys])
+--[[for _,v in pairs(keys) do
+	print(v,t[v])
+end]]--
 
-local function map(t,func)
-	local T = {}
-	for k,v in pairs(t) do
-		--table.insert(T,func(v))
-		T[k] = func(v)
-	end
-	return T
-end
-
-
-local function main()
-	io.write( "\n" .. sum( map( Fibonacci(4e6), isEven ) ) .. "\n" )
-	
-end
-
-main()
